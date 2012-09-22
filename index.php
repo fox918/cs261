6b4f100c82caedf6b82c0698d745ac8759fcd960
fox918 <oliver@oliver.lan>
2012-09-22 20:16:07 +0200

    made skeleton

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="title" content="">
+	<meta name="description" content="">
+	<meta name="author" content="Your Name Here">
+
+	<!-- IE and chrome fix -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	
+	<!-- Dublin Core Metadata : http://dublincore.org/ -->
+	<meta name="DC.title" content="Project Name">
+	<meta name="DC.subject" content="What you're about.">
+	<meta name="DC.creator" content="Who made this site.">
+	
+    <!-- Favicon -->
+	<link rel="shortcut icon" href="./img/favicon.ico">
+    <!-- Humans -->
+    <link rel="humans" href="./humans.txt">
+
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="./css/style.css">
+
+	<title></title>
+</head>
+
+<body>
+
+<div> 
+	<header>
+    	<nav>
+    	</nav>
+	</header>
+	
+	<article>
+    
+    </article>
+	
+	<aside>
+	
+	</aside>
+	
+	<footer>
+		
+	</footer>
+
+</div>
+
+<!-- Javascript -->
+<script src='./js/jquery-1.8.2.min.js'></script>
+<script src="./js/main.js"></script>
+
+</body>
+</html>
