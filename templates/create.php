59221680c95a4e3679b29766e388a8806d51a2ce
fox918 <oliver.wisler@gmail.com>
2012-10-12 13:43:34 +0200

    create.php layouting

--- templates/create.php
+++ templates/create.php
@@ -8,29 +8,39 @@ if(!isset($check)){echo 'and now for something completely different';die;}
 ?>
 <article id="create">
 <fieldset>
-    <legend>Kunde</legend>
-     <fieldset>
-        <legend>Adresse</legend>
-
-    </fieldset>
-    <fieldset>
+<legend>Kunde</legend>
+    <fieldset class='floatright'>
         <legend>Notizen</legend>
-
+            <textarea type="text" name="" id="notes" value=""></textarea>
     </fieldset>
-</fieldset>
+     <fieldset>
+        <legend id='floatleft'>Adresse</legend>
+<p class='address'>
+<span>Mobile <input type="text" name="" id="" value="" /></span>    
+<span>Telefon <input type="text" name="" id="" value="" /></span>
+<span>Strasse <input type="text" name="" id="" value="" /></span>
+<span>Ortschaft <input type="text" name="" id="" value="" /></span>
+</p>
+    </fieldset>
+    </fieldset>
 
 <fieldset>
     <legend>Auftrag</legend>
     <fieldset>
         <legend>auszufuehrnde Arbeiten</legend>
-        <p>arbeit blablablalba</p>
+        <ul>
+            <li>Arbeit blablabla <span>2h</span></li>
+            <li>Arbeit 2 blablabla <span>2h</span></li>
+        </ul>
         <input type="text" name="" id="" value="Arbeit" />
         <input type="text" name="" id="" value="Zeit" />
         <input type="button" name="" id="" value="Hinzufuegen" />
     </fieldset>
     <fieldset>
         <legend>Material</legend>
-        <p>Material blablabla</p>
+        <ul>
+            <li><span class='amount'>4</span><span class='description'>Fenster</span><span class='price'>34.23 Fr.</span></li>
+        </ul>
         <input type="text" name="" id="" value="Menge" />
         <input type="text" name="" id="" value="Was" />
         <input type="text" name="" id="" value="Kosten" />
