a07d8a8ba47859a38889be0c030b77d5ef4f33b1
CamelToe <nope@not.gonna.happen.org>
2012-10-22 15:30:54 +0200

    added some error messages

--- templates/error.php
+++ templates/error.php
@@ -51,7 +51,9 @@ $quotes = array(
     "A computer lets you make more mistakes faster than any invention in human history - with the possible exceptions of handguns and tequila.",
     "There are three kinds of death in this world.  There's heart death, there's brain death, and there's being off the network.",
     "Sorry, but you have reached the end of the internet",
-    "Q: What's tiny and yellow and very, very, dangerous? <br>A: A canary with the super-user password. "
+    "Q: What's tiny and yellow and very, very, dangerous? <br>A: A canary with the super-user password. ",
+	"You will be a winner today. Pick a fight with a four-year-old. ",
+	"question = ( to ) ? be : ! be;<br>-- Wm. Shakespeare"
 );
 
 echo $quotes[array_rand($quotes,1)];
