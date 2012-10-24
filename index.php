e47455a6315ca4ddec0e2d1503f6c2100da57b55
fox918 <fox918@nowwhere.com>
2012-10-24 19:23:18 +0200

    create multiple files and dates

--- index.php
+++ index.php
@@ -252,18 +252,50 @@ $(function() {
             $(dom).find("textarea").tinymce({
                 script_url : '../js/tinymce/jscripts/tiny_mce/tiny_mce.js',
                 theme : "simple"
-            });return false;
+            });
+            return false;
         });
 
         //add more appointments
         $("#cr_date_addfield").click(function() {
-
-        return false;
+                    var html ="<fieldset  id='date_##NUMBER##' class='date'>"+
+"                <legend>"+
+"                    Datum <input type='text' name='cr_date_##NUMBER##'/>"+
+"            <img src='./img/icons/x_alt_16x16.png' onclick='$(this).parent().parent().remove()' />" +
+"                </legend>"+
+"               <p>"+
+"            <span>"+
+"                <label for='cr_date_statime_##NUMBER##'>Startzeit</label>"+
+"                <input type='text' name='cr_date_statime_##NUMBER##' id='cr_date_statime_##NUMBER##'/>"+
+"            </span>"+
+"            <span>"+
+"                <label for='cr_date_stotime_##NUMBER##'>bis um</label>"+
+"                <input type='text' name='cr_date_stotime_##NUMBER##' id='cr_date_stotime_##NUMBER##'/>"+
+"            </span>"+
+"            <span>"+
+"                <label for='cr_date_desc_##NUMBER##'>Notiz</label>"+
+"                <input type='text' name='cr_date_desc_##NUMBER##' id='cr_date_desc_##NUMBER##'/>"+
+"            </span>"+
+"        </p>"+
+"    </fieldset>";
+            dates++;
+            html = html.replace(/##NUMBER##/g,""+dates);
+            var dom=makeDOM(html);
+            $("#calendar>div").append(dom);
+            return false;
         });
 
         //add more files
         $("#cr_file_addfield").click(function() {
-
+            var html = "<p>"+
+"    <label for='cr_file_##NUMBER##'>Datei hochladen: </label>       "+
+"    <input type='file' name='cr_file_##NUMBER##' style='width:400px'/>"+
+"    <img src='./img/icons/x_alt_16x16.png' onclick='$(this).parent().parent().remove()' />" +
+"    </p>";
+            files++;
+            html = html.replace(/##NUMBER##/g,""+files);
+            var dom=makeDOM(html);
+            $("#files>div").append(dom);
         return false;
         });
 });
