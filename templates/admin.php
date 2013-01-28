67009e8f833d8056227353cbad2ce541f322364e
CamelToe <nope@not.gonna.happen.org>
2013-01-28 18:18:27 +0100

    admin page now ready for dat javascript

--- templates/admin.php
+++ templates/admin.php
@@ -12,36 +12,61 @@ if(!defined('ACCESS'))
  */
 
 ?>
-<article id="admin">
-    <fieldset>
-        <legend>Benutzer</legend>
-<fieldset id="list">
-<legend>Benutzerliste</legend>
-    
-            <ul>
-                <li><p><span>Benutzername</span> <button type="submit">Passwort zurücksetzen</button></p> <img src="img/icons/x_alt_16x16.png" alt="" /></li>
-                <li><p><span>hans</span> <button type="submit">Passwort zurücksetzen</button></p> <img src="img/icons/x_alt_16x16.png" alt="" /></li>
-                <li><p><span>peter</span> <button type="submit">Passwort zurücksetzen</button></p> <img src="img/icons/x_alt_16x16.png" alt="" /></li>
-            </ul>
 
-</fieldset>
-        <fieldset id="newUser">
-            <legend>Benutzer erstellen</legend>
-        <p>
-        <label for="new_name">Benutzername</label>
-        <input type="text" name="new_name" id="new_name" value="" />
-        <label for="new_pw">Passwort</label>
-        <input type="password" name="new_pw" id="new_pw" value="" />    
-        <label for="new_type"> Abteilung</label>
-<select name="new_type" id="new_type">
-<option value="admin">Verwaltung</option>
-<option value="worker">Arbeiter</option>
-<option value="store">Lager</option>
-</select>
-<button type="submit">Erstellen</button>
-        </p>
-        </fieldset>
+<article id="admin">
+
+<?php
+
+
+if(!(isset($_SESSION['user']) && isset($_SESSION['auth'])))
+    die("login required");
+
+$user = new user();
+if(!$user->authenticate($_SESSION['user'], $_SESSION['auth']))
+    die("nice try; real login is required");
+
+if($user->getDepartment() == "Verwaltung")
+{
+    $db = new Database();
+    $ret = $db->run("select uId, uName from users");
+    echo "<fieldset>
+    <legend>Benutzer</legend>
+    <fieldset id=\"list\">
+    <legend>Benutzerliste</legend>
+
+    <ul>";
+    while($row = $ret->fetch_assoc())
+    {
+        $name = $row["uName"];
+        $uid = $row["uId"];
+        
+        echo "<li><p><span>$name</span> <button value=\"$uid\" type=\"submit\">Passwort zurücksetzen</button></p></li>";
+    }
+
+echo "
+    </ul>
+
     </fieldset>
+    <fieldset id=\"newUser\">
+    <legend>Benutzer erstellen</legend>
+    <p>
+    <label for=\"new_name\">Benutzername</label>
+    <input type=\"text\" name=\"new_name\" id=\"new_name\" value=\"\" />
+    <label for=\"new_pw\">Passwort</label>
+    <input type=\"password\" name=\"new_pw\" id=\"new_pw\" value=\"\" />    
+    <label for=\"new_type\"> Abteilung</label>
+    <select name=\"new_type\" id=\"new_type\">
+    <option value=\"admin\">Verwaltung</option>
+    <option value=\"worker\">Arbeiter</option>
+    <option value=\"store\">Lager</option>
+    </select>
+    <button type=\"submit\">Erstellen</button>
+    </p>
+    </fieldset>
+    </fieldset>";
+}
+
+            ?>
 <fieldset>
     <legend>Einstellungen</legend>
     <p>
