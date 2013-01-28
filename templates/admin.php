6b82b93dc1475784686b43b013f94e5375c1ec05
fox918 <fox918@nowwhere.com>
2013-01-28 17:20:24 +0100

    admin view

--- templates/admin.php
+++ templates/admin.php
@@ -12,16 +12,43 @@ if(!defined('ACCESS'))
  */
 
 ?>
-<article>
+<article id="admin">
     <fieldset>
         <legend>Benutzer</legend>
+<fieldset id="list">
+<legend>Benutzerliste</legend>
+    
             <ul>
-                <li>ROOT</li>
-                <li>HANS</li>
+                <li><p><span>Benutzername</span> <button type="submit">Passwort zurücksetzen</button></p> <img src="img/icons/x_alt_16x16.png" alt="" /></li>
+                <li><p><span>hans</span> <button type="submit">Passwort zurücksetzen</button></p> <img src="img/icons/x_alt_16x16.png" alt="" /></li>
+                <li><p><span>peter</span> <button type="submit">Passwort zurücksetzen</button></p> <img src="img/icons/x_alt_16x16.png" alt="" /></li>
             </ul>
+
+</fieldset>
+        <fieldset id="newUser">
+            <legend>Benutzer erstellen</legend>
+        <p>
+        <label for="new_name">Benutzername</label>
+        <input type="text" name="new_name" id="new_name" value="" />
+        <label for="new_pw">Passwort</label>
+        <input type="password" name="new_pw" id="new_pw" value="" />    
+        <label for="new_type"> Abteilung</label>
+<select name="new_type" id="new_type">
+<option value="admin">Verwaltung</option>
+<option value="worker">Arbeiter</option>
+<option value="store">Lager</option>
+</select>
+<button type="submit">Erstellen</button>
+        </p>
+        </fieldset>
     </fieldset>
 <fieldset>
     <legend>Einstellungen</legend>
-    <p>BLABLABLABLA</p>
+    <p>
+    <label for="pw">Mein Passwort ändern zu:</label>
+    <input type="password" name="pw" id="pw" value="" />
+    <button type="submit">ändern</button>    
+</p>
+    
 </fieldset>
 </article>
